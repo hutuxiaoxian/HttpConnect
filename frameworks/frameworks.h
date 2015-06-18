75b27093523fbb6ad530c3075d1d7820c87e443c
hutu <hutuygah@163.com>
2015-06-18 09:48:25 +0800

    优化网络请求

---
+++ frameworks/frameworks.h
@@ -0,0 +1,19 @@
+//
+//  frameworks.h
+//  frameworks
+//
+//  Created by 糊涂 on 14/10/28.
+//  Copyright (c) 2014年 hutu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for frameworks.
+FOUNDATION_EXPORT double frameworksVersionNumber;
+
+//! Project version string for frameworks.
+FOUNDATION_EXPORT const unsigned char frameworksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <frameworks/PublicHeader.h>
+
+
